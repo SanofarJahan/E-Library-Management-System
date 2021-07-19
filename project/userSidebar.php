b01f484cf6b85701ab52c2c67421277fe74f0ea4
SanofarJahan <87316028+SanofarJahan@users.noreply.github.com>
2021-07-19 16:17:39 +0530

    Delete project directory

--- project/userSidebar.php
+++
@@ -1,7 +0,0 @@
-<ul>
-	<li><a href="uhome.php">Home</a></li>
-	<li><a href="search_book.php">Search Books</a></li>
-	<li><a href="req.php">Request</a></li>
-	<li><a href="uchangepass.php">Change password</a></li>
-	<li><a href="logout.php">Logout</a></li>
-</ul>
